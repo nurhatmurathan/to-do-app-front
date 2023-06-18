373c854b7b88228ff381965bdd574f4a2448556c
nurhatmurathan <nurhatmurathan001@gmail.com>
2023-06-18 14:04:40 +0600

    add description page

--- description.js
+++ description.js
@@ -14,7 +14,7 @@ fetch(pathGet, {
             
             response.json()
                 .then(data =>{
-                    console.log(data);
+                    // console.log(data);
                     document.getElementById("user_hello").textContent = data.username; 
             
                     document.getElementById("user_email").textContent = data.email;
@@ -26,7 +26,37 @@ fetch(pathGet, {
 
 
 
+fetch("http://127.0.0.1:8000/api/user/", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            // "Authorization": "Bearer " + localStorage.getItem("access"),
+        },
+        
+    })
+    .then(response => {
+        if(response.ok){
+            
+            response.json()
+                .then(data =>{
+                    // console.log(data);
+                    const cont = document.getElementById("users");
+                    for(let i = 0; i < data.length; i++){
+                        u = document.createElement("div");
+                        a = document.createElement("a");
+                        a.textContent = i+1 + ". " + data[i].username;
+                        u.append(a);
+                        cont.append(u);
+                    }
+
+                })
+            
+        }
+    });
+
+
 var timer = setTimeout(function() {
+    console.log("Access new");
 
     refresh = {
         "refresh": localStorage.getItem("refresh")
@@ -47,7 +77,7 @@ var timer = setTimeout(function() {
             // localStorage.setItem("refresh", data.refresh);
             localStorage.setItem("access", data.access);
             // console.log(localStorage.getItem("refresh"));
-            console.log(localStorage.getItem("access"));
+            // console.log(localStorage.getItem("access"));
           })
       }else{
         console.log('Can not get access token!');
@@ -56,6 +86,6 @@ var timer = setTimeout(function() {
     .catch(error => {
         console.error(error);
     });
-  }, 5 * 60 * 1000);
+  },  60 * 1000);
 
 
